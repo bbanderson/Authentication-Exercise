b01e84926e742f5db2babe5c07f4ecd8b1ae36ff
bbanderson <tombyun@naver.com>
2020-05-17 21:36:00 +0900

    Add Passport to apply cookie and session

--- app.js
+++ app.js
@@ -4,35 +4,53 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption");
-const bcrypt = require("bcrypt");
-const md5 = require("md5");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+app.use(session({
+  secret: "ThisSecretIsEqualToCustomKeyForEncryption.",
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-// console.log(process.env.API_KEY);
-
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]})
-
-// userSchema.plugin(encrypt, { encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['age'] });
-
+userSchema.plugin(passportLocalMongoose)
 
 const User = new mongoose.model("User", userSchema);
 
-const saltRounds = 10;
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
   res.render("home")
+});
+
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.route("/register")
@@ -41,21 +59,18 @@ app.route("/register")
   })
 
   .post(function (req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      // Store hash in your password DB.
-      const newUser = new User({
-        email: req.body.username,
-        password: hash
-      });
-      
-      newUser.save(function (err) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.render("secrets");
-        }
-      });
-    });
+    User.register({username: req.body.username}, req.body.password, function (err, newUser) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        // HTML 폼에서 POST를 보내는 과정, 즉 회원가입 과정에서 에러가 없는 경우에 한해 아래 로그인세션 기억 코드(passport.authenticate())가 실행된다.
+        passport.authenticate("local") (req, res, function () {
+          // 아래 콜백함수 코드는 회원가입에 이어 그 상태로 로그인까지 연속해서 인증성공했고 + 해당 인증 내용을 바탕으로 하는 로그인 상태 쿠키 생성 성공일 때 실행된다.
+          res.redirect("/secrets")
+        })
+      }
+    })
   });
 
 app.route("/login")
@@ -64,23 +79,27 @@ app.route("/login")
   })
 
   .post(function (req, res) {
-    const userName = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: userName}, function (err, result) {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    req.login(user, function (err) {
       if (err) {
+        // const user에 해당하는 정보로 로그인할 수 없을 경우 호출되는 부분
         console.log(err);
       } else {
-        if (result) {
-          // Load hash from your password DB.
-          bcrypt.compare(password, result.password, function(err, result) {
-            // result == true
-            if (result) {
-              res.render("secrets")
-            }
-          });
-        }
+        passport.authenticate("local") (req, res, function () {
+          res.redirect("/secrets");
+        })
       }
     })
-  })
+  });
+
+app.route("/logout")
+  .get(function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 
 app.listen(3000, () => console.log("Server is connected on port 3000."))
