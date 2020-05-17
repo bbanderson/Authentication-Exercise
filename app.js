bddf8097fd470df32ec4191ed5ecc8d0563c1b5e
bbanderson <tombyun@naver.com>
2020-05-17 16:13:12 +0900

    Add MD5

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static("public"));
@@ -18,9 +19,9 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY);
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]})
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]})
 
 // userSchema.plugin(encrypt, { encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['age'] });
 
@@ -39,7 +40,7 @@ app.route("/register")
   .post(function (req, res) {
     const newUser = new User({
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
     
     newUser.save(function (err) {
@@ -58,7 +59,7 @@ app.route("/login")
 
   .post(function (req, res) {
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: userName}, function (err, result) {
       if (err) {
         console.log(err);
